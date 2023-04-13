84376253440b6ee87574ad4e778ea0eaae7f7940
Ran Pol <rnd809@gmail.com>
2023-04-13 14:31:40 -0400

    L4: Protecting Routes on the Front End.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Tips from './Tips'
 import Register from './Register'
 import Login from './Login'
 import NavBar from './NavBar'
+import ProtectedRoute from './ProtectedRoute'
 import './styles/App.css'
 
 class App extends React.Component {
@@ -30,12 +31,12 @@ class App extends React.Component {
                 <Redirect to="/login" />
               )}
             </Route>
-            <Route exact path="/diary">
+            <ProtectedRoute path="/diary" loggedIn={this.state.loggedIn}>
               <Diary />
-            </Route>
-            <Route path="/tips">
+            </ProtectedRoute>
+            <ProtectedRoute path="/tips" loggedIn={this.state.loggedIn}>
               <Tips />
-            </Route>
+            </ProtectedRoute>
             <Route path="/register">
               <Register />
             </Route>
